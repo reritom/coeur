d54fa18c891888d6fd821b899de1b625546356a8
reritom <reikudjinn@gmail.com>
2021-10-30 19:20:01 +0200

    Base config files added

---
+++ setup.py
@@ -0,0 +1,65 @@
+import re
+
+from setuptools import find_packages, setup
+
+
+def read(fname):
+    with open(fname) as fp:
+        content = fp.read()
+    return content
+
+
+EXTRAS_REQUIRE = {}
+REQUIRES = read("requirements/requirements.txt").splitlines()
+
+
+def find_version(fname):
+    """Attempts to find the version number in the file names fname.
+    Raises RuntimeError if not found.
+    """
+    version = ""
+    with open(fname) as fp:
+        reg = re.compile(r'__version__ = [\'"]([^\'"]*)[\'"]')
+        for line in fp:
+            m = reg.match(line)
+            if m:
+                version = m.group(1)
+                break
+    if not version:
+        raise RuntimeError("Cannot find version information")
+    return version
+
+
+setup(
+    name="simple-service",
+    version=find_version("src/py_services/__init__.py"),
+    description="Simple service framework",
+    author="Tomas Sheers",
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+    author_email="t.sheers@outlook.com",
+    url="https://github.com/reritom/simple-services",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    include_package_data=True,
+    install_requires=REQUIRES,
+    extras_require=EXTRAS_REQUIRE,
+    license="MIT",
+    zip_safe=False,
+    keywords="simple-services",
+    python_requires=">=3.8",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
+    test_suite="tests",
+    project_urls={
+        "Issues": "https://github.com/reritom/simple-services/issues",
+    },
+)
---
+++ src/simple_services/__init__.py
@@ -0,0 +1,5 @@
+from distutils.version import LooseVersion
+
+
+__version__ = "0.0.0"
+__version_info__ = tuple(LooseVersion(__version__).version)
