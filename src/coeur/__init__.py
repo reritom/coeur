47ab0ad921c6f760ab9f20f1e5208906c3d5c281
reritom <reikudjinn@gmail.com>
2021-10-31 12:48:05 +0100

    Version bump

--- src/coeur/__init__.py
+++ src/coeur/__init__.py
@@ -1,10 +1,9 @@
 from distutils.version import LooseVersion
 
-
-__version__ = "0.0.1"
+__version__ = "0.0.1b"
 __version_info__ = tuple(LooseVersion(__version__).version)
 
 
-from .service import Service, ServiceAction, ServiceValidationError, PermissionProtocol
+from .service import PermissionProtocol, Service, ServiceAction, ServiceValidationError
 
 __all__ = ["Service", "ServiceAction", "ServiceValidationError", "PermissionProtocol"]
